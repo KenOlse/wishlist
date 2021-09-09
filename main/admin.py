e788f2f5092f4b7b6dc4d0ae4a0e9352d928c8a2
pyshkogennadiy <pyshko.gennadiy@gmail.com>
2021-09-09 07:24:02 +0300

    first commit

---
+++ main/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+
+from .models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    '''
+        Admin View for Product
+    '''
+    list_display = ('title', 'link', 'price', 'added',)
+    list_filter = ('price',)
+    search_fields = ('title',)
+
+admin.site.register(Product, ProductAdmin)
---
+++ main/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Product(models.Model):
+	title = models.CharField('Продукт', max_length=50)
+	link = models.URLField('Інтернет-адреса',help_text='Укажіть інтернет адресу')
+	price = models.IntegerField('Ціна продукту',help_text='Укажіть ціну')
+	added = models.DateTimeField('Додано', auto_now_add=True, null=True, blank=True)
+
+	def __str__(self):
+		return self.title
+
+
+	class Meta:
+		verbose_name = 'Продукт'
+		verbose_name_plural = 'Продукти'
